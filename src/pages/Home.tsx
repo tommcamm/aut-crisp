5181ecc4a5237297ca57abe61cf21e0d522320d6
Tommaso Cammelli <tcam@itu.dk>
2024-03-27 17:13:37 +1300

    feat: new home-page and amplify fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import type { FunctionComponent } from "../common/types";
+import { Footer } from "../components/layout/footer";
+import { Navbar } from "../components/layout/navbar";
 
 export const Home = (): FunctionComponent => {
 	return (
-		<div className="bg-blue-300 font-bold w-screen h-screen flex flex-col justify-center items-center ">
-			<p className="text-white text-6xl">
-				Hello, world!
-			</p>
+		<div>
+			<Navbar/>
+			<div className="w-screen h-screen flex flex-col justify-center items-center ">
+				<p className="text-gray-600 text-5xl">
+					this page is WIP
+				</p>
+			</div>
+			<Footer/>
 		</div>
 	);
 };
