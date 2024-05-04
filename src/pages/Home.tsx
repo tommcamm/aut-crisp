9bb816bd2e2829e0a9bc90a2a4ec86f6df738fb8
Mike CHENE <mike.moana.chene@gmail.com>
2024-05-04 12:32:16 +1200

    modified home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,13 @@
 import type { FunctionComponent } from "../common/types";
 import { Footer } from "../components/layout/footer";
 import { Navbar } from "../components/layout/navbar";
+import { Carousel } from "../components/layout/carousel";
 
 export const Home = (): FunctionComponent => {
 	return (
 		<div className="flex flex-col h-screen justify-between">
 			<Navbar/>
-			<div className="w-90 flex flex-col justify-center items-center ">
-				<p className="text-gray-600 text-5xl">
-					this page is WIP
-				</p>
-			</div>
+			<Carousel />
 			<Footer/>
 		</div>
 	);
