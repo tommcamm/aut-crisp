795e8ff9d7ce88803f5783b5888da2bccaaace61
tommcamm <tomm.cammelli@outlook.it>
2024-05-06 13:07:52 +1200

    feat: 404 management

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import { createRouter } from "@tanstack/react-router";
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { NotFoundRoute, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -6,11 +7,19 @@ import { routeTree } from "./routeTree.gen.ts";
 import "./styles/tailwind.css";
 import { Amplify } from 'aws-amplify';
 import config from './amplifyconfiguration.json';
+import { Route } from "./routes/__root.ts";
+import { NotFoundPage } from "./pages/404.tsx";
 
 // Amplify configuration
 Amplify.configure(config);
 
-const router = createRouter({ routeTree });
+// 404 configuration	
+const notFoundRoute = new NotFoundRoute({
+	getParentRoute: () => Route,
+	component: () => NotFoundPage,
+  })
+
+const router = createRouter({ routeTree, notFoundRoute });
 
 declare module "@tanstack/react-router" {
 	interface Register {
---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import type { FunctionComponent } from "../common/types";
+
+export const NotFoundPage = (): FunctionComponent => {
+    return(
+        <div>
+            <h1>decreasme the there</h1>
+        </div>
+    );
+};
