c62a71e17ee4537bdaf67282391b6711d3710bd6
Mike CHENE <mike.moana.chene@gmail.com>
2024-05-12 16:22:25 +1200

    modified aboutpage

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,15 +7,17 @@ export const AboutPage = (): FunctionComponent => {
         <div className="flex flex-col h-screen justify-between">
             <Navbar />
             <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
-                <div className="flex flex-col md:flex-row">
-                    <div className="w-full md:w-1/3">
-                        <img src="https://images.unsplash.com/photo-1594581835488-0b95b8b0bacd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-48 h-48 rounded-full mx-auto" />
-                    </div>
-                    <div className="w-full md:w-2/3 pl-4 md:pl-6">
-                        <h2 className="text-2xl font-bold">About us</h2>
-                        <p></p>
-                    </div>
+            <div className="flex flex-col md:flex-row mx-auto w-full max-w-2xl">
+                <div className="w-full md:w-2/3 pl-4 md:pl-6 text-center mx-auto">
+                <h2 className="text-2xl font-bold">About us</h2>
+                <p className="mt-4">
+                    CRISP offers a comprehensive solution designed to empower both job seekers and recruiters. For job seekers, CRISP provides an intuitive platform to craft personalised profiles, complemented by the ability to upload introductory videos and traditional CVs. This multimedia approach surpasses the confines of conventional resumes, compelling candidates to showcase their skills, experiences, and personality.
+                </p>
+                <p className="mt-4">
+                    On the recruiter's front, CRISP equips hiring professionals with tools to effortlessly post job listings, rate, and engage with candidates. Furthermore, CRISP facilitates seamless communication channels, fostering meaningful interactions between recruiters and candidates throughout the recruitment journey.
+                </p>
                 </div>
+            </div>
             </div>
             <Footer />
         </div>
