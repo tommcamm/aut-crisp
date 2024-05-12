0c2094ea6230f009f15bb5eb11b51b81572c87f4
Mike CHENE <mike.moana.chene@gmail.com>
2024-05-12 16:22:13 +1200

    added aboutpage route

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -9,7 +9,7 @@ export const Footer = (): FunctionComponent=> {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-                <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                <a href="/about" className="hover:underline me-4 md:me-6">About</a>
             </li>
             <li>
                 <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
