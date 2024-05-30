99f6a65a4ba0e1e0e32b685dbf8abd41c335c986
Mike CHENE <mike.moana.chene@gmail.com>
2024-05-30 19:29:08 +1200

    modified fonts

--- src/components/layout/recruiter/recruiter-jobs-list.tsx
+++ src/components/layout/recruiter/recruiter-jobs-list.tsx
@@ -27,10 +27,10 @@ export const CreatedJobsList: FunctionComponent = () => {
   return (
     <div className="flex flex-col items-center h-screen">
     <div className="w-full max-w-xl">
-    <h1 className="text-3xl text-center mb-4">Created jobs list</h1>
+    <h1 className="text-3xl font-bold text-gray-900 text-center mb-4">Created jobs list</h1>
         <div className="overflow-y-auto h-96">
         {createdJobs.map((job) => (
-            <div key={job.id} className="bg-white shadow-md rounded px-4 py-6 mb-4 text-center">
+            <div key={job.id} className="bg-white shadow-md  font-semibold text-gray-800 rounded px-4 py-6 mb-4 text-center">
             <h2 className="text-xl">{job.title}</h2>
             {/* add other fields as necessary */}
             </div>
--- src/components/layout/recruiter/recruiter-seekers-list.tsx
+++ src/components/layout/recruiter/recruiter-seekers-list.tsx
@@ -27,11 +27,11 @@ export const CandidateList: FunctionComponent = () => {
   return (
     <div className="flex flex-col items-center h-screen ">
     <div className="w-full max-w-xl">
-    <h1 className="text-3xl text-center mb-4">Applicants list</h1>
+    <h1 className="text-3xl font-bold text-gray-900 text-center mb-4">Applicants list</h1>
         <div className="overflow-y-auto h-96">
         {candidates.map((candidate) => (
             <div key={candidate.id} className="bg-white shadow-md rounded px-4 py-6 mb-4 text-center">
-            <h2 className="text-xl">{candidate.name}</h2>
+            <h2 className="text-xl font-semibold text-gray-800">{candidate.name}</h2>
             <p>{candidate.email}</p>
             {/* add other fields as necessary */}
             </div>
